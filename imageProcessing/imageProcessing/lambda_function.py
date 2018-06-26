37ea11a2a3254647aa35f88b95d2dc3ee6f40921
Will Badr <walebadr@amazon.com>
2018-06-26 23:36:31 +1200

    fixed the template.yaml file

--- imageProcessing/imageProcessing/lambda_function.py
+++ imageProcessing/imageProcessing/lambda_function.py
@@ -13,7 +13,7 @@ start_time = time.time()
 
 client = boto3.client('lambda')
 s3 = boto3.resource('s3')
-bucket="sagemaker-serverless"
+bucket="INSERT YOUR BUCKET HERE"
 
 def upload_to_s3(channel, file, file_name):
     s3 = boto3.resource('s3')
@@ -26,7 +26,6 @@ def get_image(fname):
     img = cv2.cvtColor(cv2.imread(fname), cv2.COLOR_BGR2RGB)
     if img is None:
          return None
-
     # convert into format (batch, RGB, width, height)
     img = cv2.resize(img, (224, 224))
     img = np.swapaxes(img, 0, 2)
@@ -39,18 +38,14 @@ def lambda_handler(event, context):
     url = event['queryStringParameters']['url']
     urllib.urlretrieve(url,file_path)
     img = get_image(file_path)
-    print("--- %s time one ---" % (time.time() - start_time))
     payload = { "object": img.tolist() }
     with open('/tmp/img_numpy.json', 'w') as outfile:
         json.dump(payload, outfile)
     upload_to_s3('temp','/tmp/img_numpy.json','img_numpy.json')
-    print("--- %s time two ---" % (time.time() - start_time))
     result = client.invoke(
-    FunctionName='arn:aws:lambda:us-west-2:625616379791:function:cloud9-prediction-prediction-1E13U9IV591XN',
+    FunctionName='INSERT PREDICTION FUNCTION URL HERE',
     InvocationType='RequestResponse')
-    print("--- %s seconds ---" % (time.time() - start_time))
     results_dict = ast.literal_eval(json.loads(result['Payload'].read()))
-    #print(results_dict)
     prob = "{0:.2f}".format(results_dict.get('prob') * 100)
     msg = "There is %" + str(prob) + " chance that the picture is " + results_dict.get('label')[0]
     html_body = '<html><body><h2>' + msg + ' </h2><img src="' + url +  '" alt="flower" width="500" height="377"></body></html>'
--- prediction/prediction/lambda_function.py
+++ prediction/prediction/lambda_function.py
@@ -6,7 +6,12 @@ import boto3
 
 from collections import namedtuple
 Batch = namedtuple('Batch', ['data'])
-
+BUCKET_NAME = 'INSERT S3 BUCKET' # replace with your bucket name
+KEY = 'temp/img_numpy.json' # replace with your object key
+s3 = boto3.resource('s3')
+s3.Bucket(BUCKET_NAME).download_file(KEY, '/tmp/img_numpy.json')
+synsets = loadCategories()
+model = loadModel("Inception-BN")
 
 def loadModel(modelname, gpu=False):
         sym, arg_params, aux_params = mx.model.load_checkpoint(modelname, 0)
@@ -28,8 +33,7 @@ def loadCategories():
                 synsets.append(l.rstrip())
         return synsets
     
-synsets = loadCategories()
-model = loadModel("Inception-BN")
+
 def predict(img, model, categories, n):
         # compute the predict probabilities
         model.forward(Batch([mx.nd.array(img)]))
@@ -41,37 +45,8 @@ def predict(img, model, categories, n):
         for i in sortedprobindex[0:n]:
                 topn.append((prob[i], categories[i]))
         return topn
+        
 
-'''
-sym, arg_params, aux_params = mx.model.load_checkpoint('102flowers', 5)
-mod = mx.mod.Module(symbol=sym, context=mx.cpu(), label_names=None)
-mod.bind(for_training=False, data_shapes=[('data', (1,3,224,224))], 
-         label_shapes=mod._label_shapes)
-mod.set_params(arg_params, aux_params, allow_missing=True)
-labels = [ 'alpine sea holly','anthurium','artichoke','azalea','ball moss','balloon flower','barbeton daisy','bearded iris','bee balm','bird of paradise','bishop of llandaff','black-eyed susan','blackberry lily','blanket flower','bolero deep blue','bougainvillea','bromelia','buttercup','californian poppy','camellia','canna lily','canterbury bells','cape flower','carnation','cautleya spicata','clematis','colt\'s foot','columbine','common dandelion','corn poppy','cyclamen ','daffodil','desert-rose','english marigold','fire lily','foxglove','frangipani','fritillary','garden phlox','gaura','gazania','geranium','giant white arum lily','globe thistle','globe-flower','grape hyacinth','great masterwort','hard-leaved pocket orchi','hibiscus','hippeastrum ','japanese anemone','king protea','lenten rose','lotus','love in the mist','magnolia','mallow','marigold','mexican aster','mexican petunia','monkshood','moon orchid','morning glory','orange dahlia','osteospermum','oxeye daisy','passion flower','pelargonium','peruvian lily','petunia','pincushion flower','pink primrose','pink-yellow dahlia?','poinsettia','primula','prince of wales feathers','purple coneflower','red ginger','rose','ruby-lipped cattleya','siam tulip','silverbush','snapdragon','spear thistle','spring crocus','stemless gentian','sunflower','sweet pea','sweet william','sword lily','thorn apple','tiger lily','toad lily','tree mallow','tree poppy','trumpet creeper','wallflower','water lily','watercress','wild pansy','windflower','yellow iris']
-BUCKET_NAME = 'sagemaker-walebadr' # replace with your bucket name
-KEY = 'temp/img_numpy.json' # replace with your object key
-
-s3 = boto3.resource('s3')
-
-s3.Bucket(BUCKET_NAME).download_file(KEY, '/tmp/img_numpy.json')
-def predict(img):
-    # compute the predict probabilities
-    mod.forward(Batch([mx.nd.array(img)]))
-    prob = mod.get_outputs()[0].asnumpy()
-    # print the top-5
-    prob = np.squeeze(prob)
-    a = np.argsort(prob)[::-1]
-    i = a[0]
-    probability = prob[i]
-    label = labels[i]
-    print('probability=%f, class=%s' %(prob[i], labels[i]))
-    return probability, label
-'''
-BUCKET_NAME = 'sagemaker-serverless' # replace with your bucket name
-KEY = 'temp/img_numpy.json' # replace with your object key
-s3 = boto3.resource('s3')
-s3.Bucket(BUCKET_NAME).download_file(KEY, '/tmp/img_numpy.json')
 def lambda_handler(event, context):
     s3.Bucket(BUCKET_NAME).download_file(KEY, '/tmp/img_numpy.json')
     with open('/tmp/img_numpy.json') as json_data:
@@ -80,3 +55,4 @@ def lambda_handler(event, context):
     prob, label = predictions[0]
     response = json.dumps({"prob": prob.tolist(), "label": [label] })
     return response
+
